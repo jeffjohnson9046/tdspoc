0270a38370cfe64458af2ed822d9249c53c46019
Jeff Johnson <jeffjohnson9046@hotmail.com>
2016-06-03 11:34:43 -0700

    initial commit

---
+++ tdspoc/src/main/java/com/tdspoc/TdspocApplication.java
@@ -0,0 +1,12 @@
+package com.tdspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdspocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdspocApplication.class, args);
+	}
+}
